bacd13a677f9c9769b4a5f88af4c151a8eb3123a
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-19 21:32:18 -0500

    schedule body created, updates in readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # work-day-scheduler
+
+## Updates
+- added current day
+- created schedule body
+- added times to timeslots
+- added text box to task section
+- 3 seperate coloums for time, task, and save.
+
+## To do
+- add save button
+- add colour coding affect (past, present, future)
+- local storage
+- persistence
---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// Display the current date at the top of the page
+var currentDay = moment().format("MMM Do YY"); 
+$("#currentDay").text(currentDay);
+
+var timeBlocks = document.querySelector("#time-blocks");
+
+// Schedule body container
+var schedule = function() {
+    var timeBlocks = document.createElement("div");
+}
+
+// adding save button
+
