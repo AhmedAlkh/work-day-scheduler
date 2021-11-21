fbb22860a4f89437a46f4cebf922038bceb5ace9
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-20 19:03:52 -0500

    color coding added, updates in readme

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # work-day-scheduler
 
 ## Updates
-- added save buttons
-- CSS format applied
-
-## To do
+- made text appear black
 - add save icon to save buttons
 - add colour coding affect (past, present, future)
+
+## To do
 - local storage
 - persistence
--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,37 @@
 var currentDay = moment().format("MMM Do YY"); 
 $("#currentDay").text(currentDay);
 
-var timeBlocks = document.querySelector("#time-blocks");
+// Current time to enable color coding
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var currentTime = today + time;
 
-// Schedule body container
-var schedule = function() {
-    var timeBlocks = document.createElement("div");
-}
+// work day hours variables
+// var d = new Date();
+// var t = d.toLocaleTimeString();
 
-// adding save button
+// Global - represents color coded text boxes.
+var taskBoxEl = document.getElementsByClassName("time-block");
 
+// colour coding past, present, future
+var colorCode = function() {
+    for (var i = 0; i < taskBoxEl.length; i++) {
+        var timeSlot = parseInt(taskBoxEl[i].children[0].innerText);
+        if (timeSlot < 9) {
+            timeSlot = timeSlot + 12;
+        }
+        if (moment().hour() < timeSlot) {
+            taskBoxEl[i].children[1].className += " future";
+        } else if
+            (moment().hour() > timeSlot) {
+                taskBoxEl[i]. children[1].className += " past";
+            } else {
+                taskBoxEl[i].children[1].className += " present";
+            }
+        }
+    };
+    colorCode();
+
+/* set currentTime variable, create colorCode function with if statement.
+if currentTime is greater than 9 AM then 9 Am is equal to .past CSS class which will
+make the background color of 9 AM grey. */
